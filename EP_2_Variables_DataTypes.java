b199afefe2162fbd3db57b4a9aa6be6e77b4c119
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-28 12:06:26 +0530

    Add files via upload

---
+++ EP_2_Variables_DataTypes.java
@@ -0,0 +1,20 @@
+public class EP_2_Variables_DataTypes {
+    public static void main(String[] args){
+
+        // Primitive datatypes
+
+        int Num1 = 6;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854L;
+
+        float f = 5.8F;
+        double d = 100.67;
+
+        char c = 'r';
+
+        boolean b = true;
+
+
+    }
+}
