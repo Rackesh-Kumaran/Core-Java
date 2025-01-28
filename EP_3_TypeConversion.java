82bac0223ef5545e24c0bcdc1a567c047919c2fe
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-28 12:07:00 +0530

    Add files via upload

---
+++ EP_3_TypeConversion.java
@@ -0,0 +1,18 @@
+public class EP_3_TypeConversion {
+    public static void main(String[] args){
+
+        //type conversion or type casting:
+
+        int a = 257;
+        byte b = (byte) a;
+
+        System.out.println(b);
+
+        float f = 5.6f;
+        int t = (int) f;
+
+        System.out.println(t);
+
+
+    }
+}
